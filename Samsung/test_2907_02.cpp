cd7a0db66821b5519a71267aadc062173ca3a080
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-29 14:10:08 +0530

    character arrays

---
+++ Samsung/test_2907_02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// character arrays
+
+int main()
+{
+	char arr_str[7] = "Lokesh";
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << arr_str[i] << endl;
+	}
+	return 0;
+}
