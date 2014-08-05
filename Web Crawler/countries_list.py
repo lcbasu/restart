915ee0009e31cd294d7f24003ac20dc0ccc22e80
Lokesh Basu <lokesh.basu@gmail.com>
2014-08-05 18:39:08 +0530

    country list

---
+++ Web Crawler/countries_list.py
@@ -0,0 +1,7 @@
+countries = [['China','Beijing',1350],
+			 ['India','Delhi',1210],
+			 ['Romania','Bucharest',21],
+			 ['United States','Washington',307]]
+
+print countries[1][1]
+print ' Multiple ' + str(countries[0][2]/countries[2][2])
