f1bb87579e4f11f19565a55b6082d9c90be18b91
Lokesh Basu <lokesh.basu@gmail.com>
2014-09-06 21:00:36 +0530

    restart the triangle example

---
+++ Samsung/OpenGL ES/hello_triangle2.c
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+GLuint loadShader(GLenum type, const char *shaderSrc)
+{
+	GLuint shader;
+	GLint compiled;
+
+	shader = glCreateShader(type);
+}
+
+int main()
+{
+	return 0;
+}
