9260e8b54cccd036e8db7312f48a1d9688a1aac3
Lokesh Basu <lokesh.basu@gmail.com>
2014-09-07 20:14:14 +0530

    finish the shader loader and struct for user data

--- Samsung/OpenGL ES/hello_triangle2.c
+++ Samsung/OpenGL ES/hello_triangle2.c
@@ -2,12 +2,25 @@
 
 using namespace std;
 
+typedef struct
+{
+	GLuint programObject;
+} UserData;
+
 GLuint loadShader(GLenum type, const char *shaderSrc)
 {
 	GLuint shader;
 	GLint compiled;
 
 	shader = glCreateShader(type);
+
+	glShaderSource(shader, 1, &shaderSrc, NULL);
+
+	glCompileShader(shader);
+
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+
+	return shader;
 }
 
 int main()
