65b384783cdc52970bc78fe02c04e500c6760982
Lokesh Basu <lokesh.basu@gmail.com>
2014-09-13 11:12:45 +0530

    BFS complete

--- Samsung/test_12_09.cpp
+++ Samsung/test_12_09.cpp
@@ -6,16 +6,75 @@
 
 #include <iostream>
 #include <list>
+#include <queue>
 
 using namespace std;
 
 class Graph
 {
-	int *adjList;
+	list<int> *adjList;
 	int V;
+public:
+	Graph(int V);
+	void addEdge(int u, int v);
+	void BFS(int s);
 };
+
+Graph::Graph(int V)
+{
+	this->V = V;
+	adjList = new list<int>[V];
+}
+
+void Graph::addEdge(int u, int v)
+{
+	adjList[u].push_back(v);
+}
+
+void Graph::BFS(int src)
+{
+	bool *visited = new bool[V];
+	for (int i = 0; i < V; ++i)
+	{
+		visited[i] = false;
+	}
+
+	queue<int> que;
+
+	visited[src] = true;
+	que.push(src);
+
+	list<int>::iterator it;
+
+	while(!que.empty())
+	{
+		src = que.front();
+		cout << src << " ";
+		que.pop();
+
+		for(it = adjList[src].begin(); it != adjList[src].end(); it++)
+		{
+			if(!visited[*it])
+			{
+				visited[*it] = true;
+				que.push(*it);
+			}
+		}
+	}
+}
 
 int main()
 {
+	Graph graph(4);
+
+	graph.addEdge(0, 1);
+	graph.addEdge(0, 2);
+	graph.addEdge(1, 2);
+	graph.addEdge(2, 0);
+	graph.addEdge(2, 3);
+	graph.addEdge(3, 3);
+
+	graph.BFS(2);
+
 	return 0;
 }
