f5d957a5c224bd9a1888c987916c9a84d9564915
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-03 19:17:23 +0530

    first try

--- hunt/hackerrank/ServiceLane.cpp
+++ hunt/hackerrank/ServiceLane.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+	int N, T;
+
+	cin >> N >> T;
+
+	vector<int> width;
+	for (int k = 0; k < N; k++)
+	{
+		int tmp;
+		cin >> tmp;
+		width.push_back(tmp);
+	}
+
+	for (int l = 0; l < T; l++)
+	{
+		int i, j;
+		cin >> i >> j;
+		vector<int> path;
+		path.assign(width.begin()+i, width.begin()+i+j);
+		sort(path.begin(), path.end());
+		cout << path[0] << endl;
+	}
+
 	return 0;
 }
