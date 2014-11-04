2a28413512ca8f57ca9a546b9310c0230cf8d05c
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-04 19:12:28 +0530

    final code for Love Letter Mystery

---
+++ hunt/hackerrank/TheLLMystery.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+	while(T--)
+	{
+		string str;
+		cin >> str;
+		int i = 0;
+		int j = str.size()-1;
+		int operation = 0;
+		while(i < j)
+		{
+			operation += abs(str[i] - str[j]);
+			i++;
+			j--;
+		}
+		cout << operation << endl;
+	}
+    return 0;
+}
