043b27a7299c79538f8b90c07126948cc3c9bc61
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-04 18:52:28 +0530

    final Maximizing XOR code

--- hunt/hackerrank/MaximizingXOR.cpp
+++ hunt/hackerrank/MaximizingXOR.cpp
@@ -1,10 +1,44 @@
-#include <iostream>
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
 #include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 int main()
 {
+    int L, R;
+
+    cin >> L >> R;
+
+    int max = 0;
+
+    for (int A = L; A <= R; A++)
+    {
+        for (int B = A; B <= R; B++)
+        {
+            int val = A ^ B;
+            if (val > max)
+            {
+                max = val;
+            }
+        }
+    }
+
+    cout << max;
+
     return 0;
 }
