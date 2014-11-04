3c79cf3005364945f4b9d2cfdc471ceeb6eaae52
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-04 19:04:13 +0530

    sample template

---
+++ hunt/sample.cpp
@@ -0,0 +1,25 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    /* Your Code */
+    return 0;
+}
