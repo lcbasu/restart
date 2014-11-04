9296b80d31ed1491af7a3ca74ee863ada02307a1
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-04 07:03:10 +0530

    Utopian Tree program complete

--- hunt/hackerrank/UtopianTree.cpp
+++ hunt/hackerrank/UtopianTree.cpp
@@ -5,6 +5,31 @@
 using namespace std;
 
 int main()
-{    
+{
+    int T;
+
+    cin >> T;
+
+    for (int i = 0; i < T; ++i)
+    {
+        int treeHeight = 1;
+        int N;
+        cin >> N;
+        while(N > 0)
+        {
+            if(N > 0)
+            {
+                treeHeight = treeHeight*2;
+                N--;
+            }
+            if(N > 0)
+            {
+                treeHeight = treeHeight + 1;
+                N--;
+            }
+        }
+        cout << treeHeight << endl;
+    }
+
     return 0;
 }
