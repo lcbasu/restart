ecc49b7fbee2ba32fea1821226bd4d380410e6ac
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-05 20:14:13 +0530

    final version of Cut Sticks

--- hunt/hackerrank/CutSticks.cpp
+++ hunt/hackerrank/CutSticks.cpp
@@ -18,8 +18,41 @@
 
 using namespace std;
 
+bool  isAllZero(vector<int> v)
+{
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
-	
+    int N;
+    cin >> N;
+    
+    vector<int> numbers;
+    
+    while (N--) {
+        int temp;
+        cin >> temp;
+        numbers.push_back(temp);
+    }
+    
+    sort(numbers.begin(), numbers.end());
+    
+    while (!isAllZero(numbers)) {
+        cout << numbers.size() << endl;
+        int min = numbers[0];
+        for (int i = 0; i < numbers.size(); i++) {
+            numbers[i] = numbers[i] - min;
+        }
+        while (numbers.size() > 0 && numbers[0] == 0) {
+            numbers.erase(numbers.begin());
+        }
+    }
+    
     return 0;
 }
