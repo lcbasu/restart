7b127372b6fba94367db708e78a7bdca7f44c9db
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-06 20:12:48 +0530

    final code for Gem Stone

--- hunt/hackerrank/GemStones.cpp
+++ hunt/hackerrank/GemStones.cpp
@@ -20,5 +20,49 @@ using namespace std;
 
 int main()
 {
+    int N;
+    cin >> N;
+    
+    int occurance[26];
+    
+    for (int i = 0; i < 26; i++)
+    {
+        occurance[i] = N;
+    }
+    
+    while (N--)
+    {
+        bool visited[26];
+        for (int j = 0; j < 26; j++)
+        {
+            visited[j] = false;
+        }
+        
+        string stone;
+        cin >> stone;
+        
+        for (int k = 0; k < stone.size(); k++)
+        {
+            int index = (int)stone[k]-97;
+            if (!visited[index])
+            {
+                visited[index] = true;
+                occurance[index] = occurance[index] - 1;
+            }
+        }
+    }
+    
+    int count = 0;
+    
+    for (int i = 0; i < 26; i++)
+    {
+        if (occurance[i] == 0)
+        {
+            count++;
+        }
+    }
+    
+    cout << count;
+    
     return 0;
 }
