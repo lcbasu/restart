471a5bf4ebcfe5b4aa81e784fa063d2657e88aa5
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-07 20:25:36 +0530

    final code for Chocolate Feast

--- hunt/hackerrank/ChocolateFeast.cpp
+++ hunt/hackerrank/ChocolateFeast.cpp
@@ -1,24 +1,36 @@
-#include <map>
-#include <set>
-#include <list>
 #include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <bitset>
 #include <cstdio>
-#include <limits>
 #include <vector>
-#include <cstdlib>
-#include <numeric>
-#include <sstream>
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
+
 
 int main()
 {
+    int t,n,c,m;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>c>>m;
+        int answer=0;
+        
+        int total_wrapper = n/c;
+        int wrapper_needed = m;
+        
+        answer = total_wrapper;
+        
+        int count = 0;
+        
+        while (total_wrapper >= wrapper_needed)
+        {
+            total_wrapper = (total_wrapper-wrapper_needed) + 1;
+            count++;
+        }
+        
+        answer = answer + count;
+        
+        cout<<answer<<endl;
+    }
     return 0;
 }
