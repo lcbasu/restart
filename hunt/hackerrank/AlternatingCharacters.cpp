84016f476aed7ac9cfa9520921f7cbb0fd38006c
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-11 19:59:55 +0530

    final code for Alternating Character

--- hunt/hackerrank/AlternatingCharacters.cpp
+++ hunt/hackerrank/AlternatingCharacters.cpp
@@ -1,25 +1,36 @@
-#include <map>
-#include <set>
-#include <list>
 #include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <bitset>
 #include <cstdio>
-#include <limits>
 #include <vector>
-#include <cstdlib>
-#include <numeric>
-#include <sstream>
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
+
 
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        string str;
+        cin >> str;
+        
+        vector<char> strVec;
+        
+        for (int i = 0; i < str.size(); i++) {
+            strVec.push_back(str[i]);
+        }
+        
+        int count = 0;
+        
+        for (int i = 0; i < strVec.size()-1; i++) {
+            if (strVec[i] == strVec[i+1]) {
+                strVec.erase(strVec.begin()+i);
+                i--;
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
     return 0;
 }
