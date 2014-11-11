432ae08628aba232deba5611c650893e98cc487b
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-11 20:25:57 +0530

    final code for Manasa and Stones

--- hunt/hackerrank/MansaAndStones.cpp
+++ hunt/hackerrank/MansaAndStones.cpp
@@ -20,5 +20,35 @@ using namespace std;
 
 int main()
 {
+    int T;
+    cin >> T;
+    
+    while(T--)
+    {
+        int N, a, b;
+        
+        cin >> N >> a >> b;
+        
+        set<int> lastNumbers;
+        
+        int c1, c2;
+        
+        for (int i = 0; i < N; i++)
+        {
+            c1 = i;
+            c2 = N-1-i;
+            
+            int tmp = c1*a + c2*b;
+            
+            lastNumbers.insert(tmp);
+        }
+        set<int>::iterator it;
+        for(it = lastNumbers.begin(); it != lastNumbers.end(); it++)
+        {
+            cout << *it << " ";
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
