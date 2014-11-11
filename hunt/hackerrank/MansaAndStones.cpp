27381809b32aa4b75f0984720fc8bfdfead87c01
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-11 20:16:18 +0530

    initial setup for Manasa and Stones

---
+++ hunt/hackerrank/MansaAndStones.cpp
@@ -0,0 +1,24 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
