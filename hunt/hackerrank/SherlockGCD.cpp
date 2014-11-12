e8b6314c2bd859f85586c91a2b9b9d51daa5e843
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-12 20:20:21 +0530

    first version of Sherlock GCD

--- hunt/hackerrank/SherlockGCD.cpp
+++ hunt/hackerrank/SherlockGCD.cpp
@@ -18,7 +18,47 @@
 
 using namespace std;
 
+//the cases where {2} or {1, 2} is present is also YES for this program
+bool isSpecialPrime(int num)
+{
+    if(num == 2)
+        return false;
+    for(int i = 2; i <= num/2; i++)
+    {
+        if(num%i == 0)
+            return false;
+    }
+    return true;
+}
+
 int main()
 {
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int N;
+        cin >> N;
+        
+        vector<int> A;
+        
+        bool onePresent = false;
+        bool primePresent = false;
+        
+        for(int i = 0; i < N; i++)
+        {
+            int tmp;
+            cin >> tmp;
+            if(tmp == 1)
+                onePresent = true;
+            primePresent = isSpecialPrime(tmp);
+            A.push_back(tmp);
+        }
+        
+        if(onePresent || primePresent)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
     return 0;
 }
