300ed8da781e2758459796f6b70c9f8e21d6a0c4
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-13 18:39:26 +0530

    final code for Sherlock Square

--- hunt/hackerrank/SherlockSquares.cpp
+++ hunt/hackerrank/SherlockSquares.cpp
@@ -20,5 +20,26 @@ using namespace std;
 
 int main()
 {
+    int T;
+    
+    cin >> T;
+    
+    while(T--)
+    {
+        int A, B;
+        
+        cin >> A >> B;
+        
+        int firstSquaredNum = ceil(sqrt(A));
+        int lastSquaredNum = floor(sqrt(B));
+        
+        if(lastSquaredNum < firstSquaredNum)
+            cout << 0 << endl;
+        else if(firstSquaredNum == lastSquaredNum)
+            cout << 1 << endl;
+        else
+            cout << 2+((lastSquaredNum-firstSquaredNum)-1) << endl;
+        
+    }
     return 0;
 }
