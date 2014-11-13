8ea1294b49101d67ee066a1e1121844c11c7a231
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-13 20:28:53 +0530

    final version of Counting Sort 1

--- hunt/hackerrank/CountingSort_1.cpp
+++ hunt/hackerrank/CountingSort_1.cpp
@@ -20,5 +20,31 @@ using namespace std;
 
 int main()
 {
+    int N;
+    cin >> N;
+    
+    int arr[100];
+    
+    for(int i = 0; i < 100; i++)
+    {
+        arr[i] = 0;
+    }
+    
+    int T = N;
+    
+    while(T--)
+    {
+        int tmp;
+        cin >> tmp;
+        
+        arr[tmp] = arr[tmp]  + 1;
+    }
+    
+    for(int i = 0; i < 100; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    
     return 0;
 }
