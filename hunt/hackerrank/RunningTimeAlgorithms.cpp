0f35547a93ee984e4b93aa3d16dc922f5ebaea36
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-13 19:57:45 +0530

    final version of Running Time of Algorithm

--- hunt/hackerrank/RunningTimeAlgorithms.cpp
+++ hunt/hackerrank/RunningTimeAlgorithms.cpp
@@ -18,7 +18,44 @@
 
 using namespace std;
 
+
+int shiftCount(int *arr, int N)
+{
+    int count = 0;
+    
+    for(int j = 1; j < N; j++)
+    {
+        int key = arr[j];
+        int i = j-1;
+        while(i>=0 && arr[i] > key)
+        {
+            count++;
+            arr[i+1] = arr[i];
+            i--;
+        }
+        arr[i+1] = key;
+    }
+    
+    return count;
+}
+
 int main()
 {
+    int N;
+    
+    cin >> N;
+    
+    int *arr = new int[N];
+    
+
+    for(int i = 0; i < N; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        arr[i] = tmp;
+    }
+    
+    cout << shiftCount(arr, N);
+    
     return 0;
 }
