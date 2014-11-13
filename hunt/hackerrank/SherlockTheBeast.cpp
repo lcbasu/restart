edf7072bfef45c337f65e50e50e832726c70f256
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-13 19:12:41 +0530

    first version of The Beast

--- hunt/hackerrank/SherlockTheBeast.cpp
+++ hunt/hackerrank/SherlockTheBeast.cpp
@@ -20,5 +20,54 @@ using namespace std;
 
 int main()
 {
+    int T;
+    
+    cin >> T;
+    
+    while(T--)
+    {
+        int N;
+        
+        cin >> N;
+        
+        //we have to find m and n such that 5t + 3f = N the 3 can occur t times and 5 can occur f times
+        
+        int t = 0;
+        int f = 0;
+        
+        for(int i = 0; i < N/5; i++) //f
+        {
+            for(int j = 0; j < N/3; j++) //t
+            {
+                if(((5*i) + (3*j)) == N)
+                {
+                    if( (t == 0) && (f == 0))
+                    {
+                        f = i;
+                        t = j;
+                    }
+                    else if( i > f) //for next greater value, because more the number of 5, greater the value
+                    {
+                        f = i;
+                        t = j;
+                    }
+                }
+            }
+        }
+        
+        if( (f == 0) && (t == 0))
+        {
+            cout << -1;
+        }
+        else
+        {
+            for(int j = 0; j < 3*f; j++)
+                cout << 5;
+            for(int i = 0; i < 5*t; i++)
+                cout << 3;
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
