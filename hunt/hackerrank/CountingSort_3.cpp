57e325821a4ec6f415977afada97980690f0472c
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-14 19:15:20 +0530

    final version for Counting Sort 3

--- hunt/hackerrank/CountingSort_3.cpp
+++ hunt/hackerrank/CountingSort_3.cpp
@@ -20,6 +20,37 @@ using namespace std;
 
 int main()
 {
+    int N;
+    cin >> N;
+    int *arr = new int[N];
+    int *outArr = new int[N];
+    int *count = new int[100];
+    
+    for(int i = 0; i < 100; i++)
+    {
+        count[i] = 0;
+    }
+    
+    for(int i = 0; i < N; i++)
+    {
+        int tmp;
+        string s;
+        cin >> tmp >> s;
+        arr[i] = tmp;
+        count[tmp] = count[tmp] + 1;
+    }
+    
+    for(int i = 1; i < 100; i++)
+    {
+        count[i] = count[i-1] + count[i];
+    }
+    
+    for(int i = 0; i < 100; i++)
+    {
+        cout << count[i] << " ";
+    }
+    
+    cout << endl;
     
     return 0;
 }
