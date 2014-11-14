7a34d86b34061b23e71a3e6c286c49e2632c8780
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-14 19:09:07 +0530

    first attempt for Counting Sort 2

--- hunt/hackerrank/CountingSort_2.cpp
+++ hunt/hackerrank/CountingSort_2.cpp
@@ -20,5 +20,47 @@ using namespace std;
 
 int main()
 {
+    int N;
+    cin >> N;
+    int *arr = new int[N];
+    int *outArr = new int[N];
+    int *count = new int[100];
+    
+    for(int i = 0; i < 100; i++)
+    {
+        count[i] = 0;
+    }
+    
+    for(int i = 0; i < N; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        arr[i] = tmp;
+        count[tmp] = count[tmp] + 1;
+    }
+    
+    for(int i = 1; i < 100; i++)
+    {
+        count[i] = count[i-1] + count[i];
+    }
+    
+    for(int i = N-1; i >= 0; i--)
+    {
+        int finalIndex = count[arr[i]];
+        outArr[finalIndex] = arr[i];
+        count[arr[i]] = count[arr[i]] - 1;
+    }
+    
+    for(int i = 0; i < N; i++)
+    {
+        cout << outArr[i] << " ";
+    }
+    
+    cout << endl;
+    
+    
+    
+    
+    
     return 0;
 }
